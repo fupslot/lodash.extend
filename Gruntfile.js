e5b8e1b0117e7dbea7daa73e99bd9f233e74bfc7
Eugene Brodsky <fupslot@gmail.com>
2014-05-18 10:33:00 +0300

    existy, truthy, cat, construct, select, rename, as

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  //config
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          host: 'localhost',
+          port: 9999,
+          base: 'dist',
+          open: {
+            target: 'http://localhost:9999'
+          }
+        }
+      }
+    },
+
+    concat: {
+      app: {
+        options: {
+          process: function(src, path) {
+            return '!(function(lodash){\r\n'+src+'\r\n})(_);';
+          }
+        },
+        src: ['src/**/*.*'],
+        dest: 'dist/fupslot.lodash.extend.js'
+      }
+    },
+
+    uglify: {
+      app: {
+        src: ['./dist/fupslot.lodash.extend.js'],
+        dest: './dist/fupslot.lodash.extend.min.js'
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['src/**/*.*'],
+        tasks: ['concat'],
+        options: {
+          livereload: {
+            port: 9001,
+          }
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['connect:server','watch']);
+  grunt.registerTask('package', ['concat:app','uglify:app']);
+};
---
+++ dist/fupslot.lodash.extend.js
@@ -0,0 +1,50 @@
+!(function(lodash){
+
+function existy(x) { return x != null };
+function truthy(x) { return (x !== false) && existy(x) };
+
+
+function cat() {
+    var head = lodash.first(arguments);
+    if (existy(head)) return head.concat.apply(head, lodash.rest(arguments)); 
+    else return [];
+}
+
+function construct(head, tail) { 
+    return cat([head], lodash.toArray(tail));
+}
+
+function project(table, keys) {
+    return lodash.map(table, function(obj) {
+        return lodash.pick.apply(null, construct(obj, keys));
+    });
+};
+
+function rename(obj, newNames) {
+    return lodash.reduce(newNames, function(o, nu, old) {
+        if (lodash.has(obj, old)) {
+            o[nu] = obj[old];
+            return o;
+        }
+        else return o;
+    },
+    lodash.omit.apply(null, construct(obj, lodash.keys(newNames))));
+};
+
+function as(table, newNames) {
+    return _.map(table, function(obj) {
+        return rename(obj, newNames);
+    });
+};
+
+lodash.mixin({
+    'existy': existy,
+    'truthy': truthy,
+    'cat': cat,
+    'construct':construct,
+    'project':project,
+    'rename':rename,
+    'as':as
+});
+
+})(_);
